c226ad085e0eafb78fc2a3fd5182effe0206180f
Defendi <tiagodefendidasilva@gmail.com>
2025-12-11 22:10:18 -0300

    implementação da lógica para obter módulos e classes no serviço de classes

--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -2,5 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ClassesService {
-    // Lógica relacionada às classes pode ser adicionada aqui
+    // MODULE =================================================================================
+
+    async getAllModules() {
+        // Lógica para obter todos os módulos
+    }
+
+    async getModuleById(moduleId: string) {
+        // Lógica para obter um módulo específico pelo ID
+    }
+
+    // CLASSES ================================================================================
+
+    async getAllClassesInModule(moduleId: string) {
+        // Lógica para obter todas as classes
+    }
+
+    async getClassByIdInModule(moduleId: string, classId: string) {
+        // Lógica para obter uma classe específica pelo ID
+    }
 }
